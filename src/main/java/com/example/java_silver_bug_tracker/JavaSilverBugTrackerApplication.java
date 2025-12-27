1fb84634b85ace4571844e3a61cdd48c86b517fe
ハタハラリョウタ <hatahara@hatahararyoutanoMacBook-Air.local>
2025-12-28 01:00:55 +0900

    環境構築

---
+++ src/main/java/com/example/java_silver_bug_tracker/JavaSilverBugTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.example.java_silver_bug_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class JavaSilverBugTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSilverBugTrackerApplication.class, args);
+	}
+
+}
