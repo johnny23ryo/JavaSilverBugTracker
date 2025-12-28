b08da3b0b60cd44d0a69e941bf7c5424e6310720
ハタハラリョウタ <hatahara@hatahararyoutanoMacBook-Air.local>
2025-12-28 17:52:51 +0900

    README追加

---
+++ src/test/java/com/example/java_silver_bug_tracker/JavaSilverBugTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_silver_bug_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSilverBugTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
