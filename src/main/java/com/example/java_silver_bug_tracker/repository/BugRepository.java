7413f525ae2c9cfcd4b4645a9fa9cafecdc3aa56
ハタハラリョウタ <hatahara@hatahararyoutanoMacBook-Air.local>
2026-01-01 22:22:55 +0900

    リポジトリ

--- src/main/java/com/example/java_silver_bug_tracker/repository/BugRepository.java
+++ src/main/java/com/example/java_silver_bug_tracker/repository/BugRepository.java
@@ -1,4 +1,19 @@
 package com.example.java_silver_bug_tracker.repository;
+import com.example.java_silver_bug_tracker.model.Bug;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.ArrayList;
 
+@Repository
 public class BugRepository {
+    private static final List<Bug> bugList = new ArrayList<>();
+
+    public void save(Bug bug) {
+        bugList.add(bug);
+
+    }
+
+    public List<Bug> findAll(){
+        return bugList;
+    }
 }
